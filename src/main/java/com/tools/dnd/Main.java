c48cd5c709cc0724773857759501eb832fbe827f
AY1048576 <github@me.averyyanowitz.com>
2025-07-18 22:57:28 -0400

    initial commit

---
+++ src/main/java/com/tools/dnd/Main.java
@@ -0,0 +1,7 @@
+package com.tools.dnd;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/tools/dnd/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.tools.dnd;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void assertsTrue() {
+        assertTrue(true);
+    }
+
+}
